e8946ed65d286dae77996a6e6e64b067043ff08a
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-08-18 18:52:10 +0500

    test 2

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 
 export const getDataFromToken = (req) => {
     try {
+        console.log("came 1");
         const token = req.cookies.get("token")?.value || '';
+        console.log("came 2", token);
         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
         
         // it also has username and email in decoded token because we set these at the login time
